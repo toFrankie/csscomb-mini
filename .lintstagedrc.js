de11dd119e5fe572aff3f1e738948bde30bc97b3
Frankie <1426203851@qq.com>
2021-05-26 15:46:50 +0800

    fix: 修复遗漏问题

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  '*.{js}': 'eslint --fix',
+  '*.{js}': ['prettier --wirte', 'eslint --fix'],
   '*.{css,wxss,acss}': filenames => [
     `prettier --write ${filenames.join(' ')}`,
     `gulp csscombTask  --path '${filenames.join(',')}'`
   ],
-  '*.{json,wxml,axml}': 'prettier --write'
+  '*.{json,wxml,axml,swan}': 'prettier --write'
 }
